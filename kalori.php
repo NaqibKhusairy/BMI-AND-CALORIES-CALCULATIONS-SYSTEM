fe0ec13a4eb9c70c3c92dcf0448853c16409ce47
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-08 20:39:18 +0800

    Update kalori.php

--- kalori.php
+++ kalori.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>CALORIES CALCULATOR</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-        <link rel="icon" href="icon.png" type="image/png">
+        <title>CALORIES CALCULATOR</title> <!--The title -->
+        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
+        <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
     <body>
-        <h1>Calorie Calculator</h1>
-        <form action="kalori_result.php" method="post" target="_blank">
+        <h1>Calorie Calculator</h1> <!--The header -->
+        <form action="kalori_result.php" method="post" target="_blank"> <!--The form -->
             Age: <input type="text" name="age"><br><br>
             Gender:
             <input type="radio" name="sex_type" value="male"> Male
@@ -21,7 +21,7 @@
                 <option value="1.55">Moderately Active</option>
                 <option value="1.725">Heavy (Very Active)</option>
             </select><br><br>
-            <input type="submit" value="Calculate">
+            <input type="submit" value="Calculate"> <!--The Calculate button -->
         </form>
     </body>
 </html>
