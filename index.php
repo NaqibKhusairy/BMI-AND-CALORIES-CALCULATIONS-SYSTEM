6da5176bd4936665cae88ede88de355f325f84a1
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-08 20:38:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>BMI & CALORIES CALCULATIONS SYSTEM</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-        <link rel="icon" href="icon.png" type="image/png">
+        <title>BMI & CALORIES CALCULATIONS SYSTEM</title> <!--The title -->
+        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
+        <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
     <body>
-        <h1>Please Select Calculator</h1>
+        <h1>Please Select Calculator</h1> <!--The header -->
         <ul class="button-list">
-            <li class="button button-bmi"><a href="bmi.php">BMI Calculator</a></li>
-            <li class="button button-kalori"><a href="kalori.php">Calories Calculator</a></li>
+            <li class="button button-bmi"><a href="bmi.php">BMI Calculator</a></li> <!--button to BMI Calculator -->
+            <li class="button button-kalori"><a href="kalori.php">Calories Calculator</a></li> <!--button to Calories Calculator -->
         </ul>
     </body>
 </html>
