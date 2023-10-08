b6e4b252f45bd17cab7f42dee2bda03e13eff20b
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-08 20:39:32 +0800

    Update kalori_result.php

--- kalori_result.php
+++ kalori_result.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>CALORIES CALCULATOR - RESULT</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-        <link rel="icon" href="icon.png" type="image/png">
+        <title>CALORIES CALCULATOR - RESULT</title> <!--The title -->
+        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
+        <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
     <body>
-        <h1>Calorie Calculator Results</h1>
+        <h1>Calorie Calculator Results</h1> <!--The header -->
         
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $age = $_POST["age"];
-                $gender = $_POST["sex_type"];
-                $height = $_POST["height"];
-                $weight = $_POST["weight"];
-                $activity = $_POST["activity"];
+                $age = $_POST["age"]; // get age from the form
+                $gender = $_POST["sex_type"]; // get sex_type from the form
+                $height = $_POST["height"]; // get height from the form
+                $weight = $_POST["weight"]; // get weight from the form
+                $activity = $_POST["activity"]; // get activity from the form
 
                 // Calculate daily calorie needs
                 if ($gender == "male") {
@@ -23,9 +23,9 @@
                     $bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
                 }
 
-                $total_calories = $bmr * $activity;
+                $total_calories = $bmr * $activity; // formula to calculate total_calories
 
-                echo "<p>Your Daily Calorie Needs: " . number_format($total_calories, 2) . " calories per day</p>";
+                echo "<p>Your Daily Calorie Needs: " . number_format($total_calories, 2) . " calories per day</p>"; // show the result
             }
         ?>
     </body>
