654fed1d5eb9c749cc98e6d4640e7bd864f4c004
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-08 20:38:22 +0800

    Update bmi.php

--- bmi.php
+++ bmi.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>BMI CALCUATOR</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-        <link rel="icon" href="icon.png" type="image/png">
+        <title>BMI CALCUATOR</title> <!--The title -->
+        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
+        <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
     <body>
-        <h1>BMI CALCUATOR</h1>
-        <form action="bmi_result.php" method="post" target="_blank">
+        <h1>BMI CALCUATOR</h1> <!--The header -->
+        <form action="bmi_result.php" method="post" target="_blank"> <!--The form -->
             Height (cm): <input type="text" name="height"><br>
             Weight (kg): <input type="text" name="weight"><br>
-            <input type="submit" value="Hitung">
+            <input type="submit" value="Calculate"> <!--The Calculate button -->
         </form>
     </body>
 </html>
