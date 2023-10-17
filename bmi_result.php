fc24210c7be4f416969fed7446af3b63940b700c
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-17 20:43:21 +0800

    update

--- bmi_result.php
+++ bmi_result.php
@@ -2,39 +2,76 @@
 <html>
     <head>
         <title>BMI CALCUATOR - RESULT</title> <!--The title -->
-        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
+        <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link to Cascading Style Sheets file-->
         <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
-    <body>
-        <h1>BMI Calculator Results</h1> <!--The header -->
-        
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $height = $_POST["height"] / 100; // Convert height from cm to meters
-                $weight = $_POST["weight"]; // get weight from the form
-                $bmi = $weight / ($height * $height); // formula to calculatr bmi
+    <body onload="window.print();" accesskey="p">
+        <center>
+            <form action="next.php" method="post">
+            <h1>BMI Calculator Results</h1> <!--The header -->
+            
+            <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $height = $_POST["height"] / 100; // Convert height from cm to meters
+                    $weight = $_POST["weight"]; // get weight from the form
+                    $bmi = $weight / ($height * $height); // formula to calculate BMI
 
-                echo "<p>Body Mass Index (BMI): " . number_format($bmi, 2) . "</p>"; // show the result
+                    $interpretation = ""; // Initialize interpretation variable
+                    if ($bmi < 16) {
+                        $interpretation = "(Severe Thinness)";
+                        $normal_weight = ($height * $height) * 18.6; // Calculate the weight needed to achieve Normal BMI
+                    } elseif ($bmi >= 16 && $bmi <= 17) {
+                        $interpretation = "(Moderate Thinness)";
+                        $normal_weight = ($height * $height) * 18.6; // Calculate the weight needed to achieve Normal BMI
+                    } elseif ($bmi > 17 && $bmi < 18.6) {
+                        $interpretation = "(Mild Thinness)";
+                        $normal_weight = ($height * $height) * 18.6; // Calculate the weight needed to achieve Normal BMI
+                    } elseif ($bmi >= 18.6 && $bmi <= 25) {
+                        $interpretation = "(Normal)";
+                    } elseif ($bmi > 25.1 && $bmi < 30) {
+                        $interpretation = "(Overweight)";
+                        $normal_weight = ($height * $height) * 25; // Calculate the weight needed to achieve Normal BMI
+                    } elseif ($bmi >= 30 && $bmi <= 35) {
+                        $interpretation = "(Obese Class I)";
+                        $normal_weight = ($height * $height) * 25; // Calculate the weight needed to achieve Normal BMI
+                    } elseif ($bmi > 35 && $bmi <= 40) {
+                        $interpretation = "(Obese Class II)";
+                        $normal_weight = ($height * $height) * 25; // Calculate the weight needed to achieve Normal BMI
+                    } else {
+                        $interpretation = "(Obese Class III)";
+                        $normal_weight = ($height * $height) * 25; // Calculate the weight needed to achieve Normal BMI
+                    }
 
-                // Interpretation of BMI
-                if ($bmi < 16) {
-                    echo "<p>(Severe Thinness)</p>";
-                } elseif ($bmi >= 16 && $bmi <= 17) {
-                    echo "<p>(Moderate Thinness)</p>";
-                } elseif ($bmi > 17 && $bmi < 18.6) {
-                    echo "<p>(Mild Thinness)</p>";
-                } elseif ($bmi >= 18.6 && $bmi <= 25) {
-                    echo "<p>(Normal)</p>";
-                } elseif ($bmi > 25.1 && $bmi < 30) {
-                    echo "<p>(Overweight)</p>";
-                } elseif ($bmi >= 30 && $bmi <= 35) {
-                    echo "<p>(Obese Class I)</p>";
-                } elseif ($bmi > 35 && $bmi <= 40) {
-                    echo "<p>(Obese Class II)</p>";
-                } else {
-                    echo "<p>(Obese Class III)</p>";
+                    if ($interpretation != "(Normal)") {
+                        // JavaScript code to display the result in a pop-up
+                        echo "
+                            <script>
+                                alert('Body Mass Index (BMI): " . number_format($bmi, 2) . " " . $interpretation . 
+                                "\\nTo achieve a Normal BMI, you need to weigh approximately " . number_format($normal_weight, 2) ."');
+                            </script>
+                        ";
+
+                        echo "<p>Body Mass Index (BMI): " . number_format($bmi, 2) . "</p>"; // show the result
+                        echo "<p>$interpretation</p>";
+                        echo "<p>To achieve a Normal BMI, you need to weigh approximately " . number_format($normal_weight, 2) . " kg.</p>";
+                    }
+                    else{
+                
+                        // JavaScript code to display the result in a pop-up
+                        echo "
+                            <script>
+                                alert('Body Mass Index (BMI): " . number_format($bmi, 2) . " " . $interpretation . "');
+                            </script>
+                        ";
+    
+                        echo "<p>Body Mass Index (BMI): " . number_format($bmi, 2) . "</p>"; // show the result
+                        echo "<p>$interpretation</p>";
+                        
+
+                    }
                 }
-            }
-        ?>
+            ?>
+            </form>
+        </center>
     </body>
 </html>
--- kalori_result.php
+++ kalori_result.php
@@ -5,28 +5,50 @@
         <link rel="stylesheet" type="text/css" href="styles.css"> <!--The link Cascading Style Sheets file-->
         <link rel="icon" href="icon.png" type="image/png"> <!--The Icon -->
     </head>
-    <body>
-        <h1>Calorie Calculator Results</h1> <!--The header -->
-        
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $age = $_POST["age"]; // get age from the form
-                $gender = $_POST["sex_type"]; // get sex_type from the form
-                $height = $_POST["height"]; // get height from the form
-                $weight = $_POST["weight"]; // get weight from the form
-                $activity = $_POST["activity"]; // get activity from the form
+    <body onload="window.print();" accesskey="p">
+        <center>
+            <form action="next.php" method="post">
+                <h1>Calorie Calculator Results</h1> <!--The header -->
+                
+                <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $age = $_POST["age"]; // get age from the form
+                        $gender = $_POST["sex_type"]; // get sex_type from the form
+                        $height = $_POST["height"]; // get height from the form
+                        $weight = $_POST["weight"]; // get weight from the form
+                        $activity = $_POST["activity"]; // get activity from the form
 
-                // Calculate daily calorie needs
-                if ($gender == "male") {
-                    $bmr = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
-                } elseif ($gender == "female") {
-                    $bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
-                }
+                        // Calculate daily calorie needs
+                        if ($gender == "male") {
+                            $bmr = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+                        } elseif ($gender == "female") {
+                            $bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+                        }
 
-                $total_calories = $bmr * $activity; // formula to calculate total_calories
+                        $total_calories = $bmr * $activity; // formula to calculate total_calories
 
-                echo "<p>Your Daily Calorie Needs: " . number_format($total_calories, 2) . " calories per day</p>"; // show the result
-            }
-        ?>
+                        // Food suggestions based on calorie needs
+                        $food_suggestions = "";
+                        if ($total_calories < 1200) {
+                            $food_suggestions = "You may want to consume foods high in healthy fats, lean proteins, and complex carbohydrates such as avocados, lean chicken breast, and whole grains.";
+                        } elseif ($total_calories >= 1200 && $total_calories < 1800) {
+                            $food_suggestions = "Consider a balanced diet with a mix of proteins, vegetables, fruits, and whole grains to meet your calorie needs.";
+                        } else {
+                            $food_suggestions = "To meet your high calorie needs, include a variety of foods, including lean proteins, dairy products, and plenty of fruits and vegetables.";
+                        }
+
+                        // JavaScript code to display the result in a pop-up
+                        echo "
+                            <script>
+                                alert('Your Daily Calorie Needs: " . number_format($total_calories, 2) . " calories per day' + '\\nFood Suggestions: " . $food_suggestions . "');
+                            </script>
+                        ";
+
+                        echo "<p>Your Daily Calorie Needs: " . number_format($total_calories, 2) . " calories per day</p>"; // show the result
+                        echo "<p>Food Suggestions: " . $food_suggestions . "</p>";
+                    }
+                ?>
+            </form>
+        </center>
     </body>
 </html>
